9d43e23b3d72f12c6cf281286435a07cb26bb083
mcavagnolo <mcavagnolo@gmail.com>
2025-12-09 13:14:24 -0800

    Setup GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
 import Home from './pages/Home';
